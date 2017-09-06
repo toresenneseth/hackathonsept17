ae2d66bf737bd483de9efd80a7f6ef6aa348c456
Tore Senneseth <tore.senneseth@outlook.com>
2017-09-06 23:03:59 +0200

    Add comment

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -16,6 +16,7 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // some change
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
